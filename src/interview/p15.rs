a7f0385f544b97cf44879555b0fb77691de7e8fa
han <13766804794@163.com>
2024-11-21 20:10:08 +0800

    feat p15 end

--- src/interview/p15.rs
+++ src/interview/p15.rs
@@ -3,20 +3,27 @@ use crate::Solution;
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort();
-        let ans: Vec<Vec<i32>> = vec![];
+        let mut ans: Vec<Vec<i32>> = vec![];
         let n = nums.len();
         for (i, num) in nums.iter().enumerate() {
-            let j = i + 1;
+            let mut j = i + 1;
             let mut k = n - 1;
-            while k < j {
+            while k > j {
                 let s = num + nums[j] + nums[k];
                 if s > 0 {
                     k -= 1;
+                } else if s < 0 {
+                    j += 1;
+                } else {
+                    let result = vec![*num, nums[j], nums[k]];
+                    if !ans.contains(&result) {
+                        ans.push(vec![*num, nums[j], nums[k]]);
+                    }
+                    j += 1;
                 }
             }
         }
-
+        println!("{:?}", ans);
         ans
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -27,5 +27,5 @@ mod search {
 }
 
 fn main() {
-    Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]);
+    Solution::three_sum(vec![-2, 0, 1, 1, 2]);
 }
